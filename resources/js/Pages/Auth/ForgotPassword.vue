8cc26cc7ab39142774726ad7e90793145820e8cb
Floresco <dossouyovojoys@gmail.com>
2022-07-05 15:37:19 +0000

    Register and Login with username or email

---
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -0,0 +1,53 @@
+<script setup>
+import BreezeButton from '@/Components/Button.vue';
+import BreezeGuestLayout from '@/Layouts/Guest.vue';
+import BreezeInput from '@/Components/Input.vue';
+import BreezeLabel from '@/Components/Label.vue';
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+import { Head, useForm } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    status: String,
+});
+
+const form = useForm({
+    login: '',
+});
+
+const submit = () => {
+    form.post(route('password.email'));
+};
+</script>
+
+<template>
+    <BreezeGuestLayout>
+
+        <Head title="Forgot Password" />
+
+        <div class="mb-4 text-sm text-gray-600">
+            Forgot your password? No problem. Just let us know your email address or username and we will email you a
+            password reset
+            link that will allow you to choose a new one.
+        </div>
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <BreezeValidationErrors class="mb-4" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <BreezeLabel for="login" value="Email / Username" />
+                <BreezeInput id="login" type="text" class="mt-1 block w-full" v-model="form.login" required autofocus
+                    autocomplete="username" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <BreezeButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Email Password Reset Link
+                </BreezeButton>
+            </div>
+        </form>
+    </BreezeGuestLayout>
+</template>
